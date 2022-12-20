f59d34c168b317d0d3bdd658cb7e35880cd1302f
Kotak-Prerit <kotakprerit7@gmail.com>
2022-12-20 17:36:53 +0530

    last Commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const capturecolor = () => {
     const randomNum = Math.floor(Math.random() * 16777215);
     const randomHex = "#" + randomNum.toString(16);
     document.body.style.background = randomHex;
+    document.body.style.backgroundImage = "url('img/main-bg.png')";
+    document.body.style.backgroundSize = "cover";
     document.getElementById("HexCode").innerText = randomHex;
 
     navigator.clipboard.writeText(randomHex);
